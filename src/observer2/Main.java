162a0e4eec14be28e11ea0dc42dcfff612877478
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-06-07 12:36:16 +0200

    added the notes or comments

--- src/observer2/Main.java
+++ src/observer2/Main.java
@@ -8,6 +8,13 @@ public class Main {
         new FaxSend(tasr);
         new FbSend(tasr);
 
+        //nastavenie priority cez setter a v Tasr mam metodu, ktora
+        //overuje prioritu a porovnava akej je instancie a ake je cislo
+        //a na zaklade toho sa vypise
+        //ak je 1 vsetke ju vzpisu
+        //ak je 4 nikto
+        //ak je 2 tak email a sms
+        //ak je 3 tak Fb a Fax
         tasr.setPriority(1);
         tasr.setMessage("Ahoj");
     }
